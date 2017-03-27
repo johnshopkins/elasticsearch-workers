fd78fabd889e5563cffcdd78df109ce34dca815f
Jen Wachter <jenleighkelly@gmail.com>
2017-03-27 09:16:41 -0400

    Created basic workers.

---
+++ src/Director.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace ElasticsearchWorkers;
+
+use Secrets\Secret;
+
+class Director
+{
+	public function __construct($settings = array())
+	{
+    // logger exchange
+		$this->logger = $settings["logger"];
+
+    // plugin namespace (hub, hubapi, jhu)
+		$this->namespace = $settings["namespace"];
+
+    // function that tests whether an item can be pushed to elasticsearch
+    // arguments: id, type
+		$this->saveTest = $settings["saveTest"];
+
+    $this->setupGearmanClient($settings["servers"]);
+	}
+
+  protected function setupGearmanClient($servers)
+  {
+    $this->gearmanClient = new \GearmanClient();
+
+    foreach ($servers as $server) {
+      $this->gearmanClient->addServer($server->hostname);
+    }
+  }
+
+	public function saved($id, $type)
+	{
+    $saved = call_user_func($this->saveTest, $id);
+
+    if ($saved) {
+      $this->put($id, $type);
+    } else {
+      $this->remove($id, $type);
+    }
+	}
+
+	public function put($id, $type)
+	{
+		return $this->gearmanClient->doBackground("{$this->namespace}_elasticsearch_put", json_encode(array(
+			"id" => $id,
+			"type" => $type
+		)));
+	}
+
+	public function remove($node, $type)
+	{
+    return $this->gearmanClient->doBackground("{$this->namespace}_elasticsearch_delete", json_encode(array(
+      "id" => $id,
+			"type" => $type
+		)));
+	}
+
+	public function reindex()
+	{
+		return $this->gearmanClient->doNormal("{$this->namespace}_elasticsearch_reindex", json_encode(array()));
+	}
+
+}
